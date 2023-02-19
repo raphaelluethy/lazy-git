de6e9908dfdc6bae2ca224a966d4285c1db0f4ff
Raphael Lüthy <raphael@famluethy.ch>
2023-02-20 00:12:29 +0100

    Added vimtex, treesitter context and changed some settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,12 @@
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Esc" })
 vim.keymap.set("c", "<C-c>", "<Esc>", { desc = "Esc" })
 vim.keymap.set("v", "<C-c>", "<Esc>", { desc = "Esc" })
-
+--
 vim.keymap.set("n", "<C-b>", "<cmd>Neotree source=filesystem toggle=true <CR>", { silent = true })
+
+vim.keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume" }
+)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,10 +13,33 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = true
 vim.opt.termguicolors = true
 
 vim.opt.termguicolors = true
 vim.g.sonokai_style = "maia"
 vim.g.sonokai_better_performance = 1
 vim.g.sonokai_transparent_background = 2
+
+-- vimtex
+vim.g.vimtex_view_method = "skim"
+vim.g.tex_flavor = "latex" -- Default tex file format
+vim.g.vimtex_view_general_viewer = "skim"
+vim.g.vimtex_view_skim_sync = 1 -- Value 1 allows forward search after every successful compilation
+vim.g.vimtex_view_skim_activate = 1 -- Value 1 allows change focus to skim after command `:VimtexView` is given
+
+vim.g.vimtex_view_forward_search_on_start = false
+vim.g.vimtex_toc_config = {
+  mode = 1,
+  fold_enable = 0,
+  hide_line_numbers = 1,
+  resize = 0,
+  refresh_always = 1,
+  show_help = 0,
+  show_numbers = 1,
+  split_pos = "leftabove",
+  split_width = 30,
+  tocdeth = 3,
+  indent_levels = 1,
+  todo_sorted = 1,
+}
+vim.g.maplocalleader = ","
